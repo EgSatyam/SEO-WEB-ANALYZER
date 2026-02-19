c8245b07139fb0da070ee4bb4f9c209888839b63
EgSatyam <satyamsahu111213@gmail.com>
2026-02-19 21:14:54 +0530

    deployment ready

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,12 +6,14 @@ import authRoutes from './routes/auth.routes.js';
 import reportRoutes from './routes/report.routes.js';
 import { errorHandler } from './middleware/error.middleware.js';
 import { apiLimiter } from './middleware/rateLimit.middleware.js';
+import path from 'path';
 
 const app = express();
 
 const corsOrigin = process.env.CORS_ORIGIN || 'http://localhost:5173';
 app.use(helmet({ crossOriginResourcePolicy: { policy: 'cross-origin' } }));
 app.use(cors({ origin: corsOrigin, credentials: true }));
+
 app.use(cookieParser());
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true, limit: '1mb' }));
@@ -19,6 +21,13 @@ app.use('/api', apiLimiter);
 
 app.use('/api/auth', authRoutes);
 app.use('/api/reports', reportRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'))
+    })
+}
 
 app.use(errorHandler);
 
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.VITE_API_URL || '/api';
+const baseURL = import.meta.env.VITE_API_URL === 'development' ? 'http://localhost:5000/api':'/api';
 
 let accessToken = null;
 
